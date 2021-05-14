c7b1cdce4a5bad3907ae5c486d424cdf27e194d6
uanderns <uanderns88@gmail.com>
2021-05-14 16:51:25 -0300

    entities

--- backend/src/main/java/com/cakedeliver/cakedeliver/entities/AvaliacaoPedido.java
+++ backend/src/main/java/com/cakedeliver/cakedeliver/entities/AvaliacaoPedido.java
@@ -1,5 +1,9 @@
 package com.cakedeliver.cakedeliver.entities;
 
+import com.cakedeliver.cakedeliver.enums.ClassificacaoAvaliacao;
+
 public class AvaliacaoPedido {
 
+	private String comentario;
+	private ClassificacaoAvaliacao classificacao;
 }
--- backend/src/main/java/com/cakedeliver/cakedeliver/enums/ClassificacaoAvaliacao.java
+++ backend/src/main/java/com/cakedeliver/cakedeliver/enums/ClassificacaoAvaliacao.java
@@ -2,4 +2,7 @@ package com.cakedeliver.cakedeliver.enums;
 
 public enum ClassificacaoAvaliacao {
 
+	HORRIVEL, RUIM, RAZOAVEL, MUITO_BOM, EXCELENTE;
+	
+	// Classificação em estrelas de 1-Horrivel, 2- Ruim, 3- Razoavel, 4- Muito bom e 5- Excelente 
 }
